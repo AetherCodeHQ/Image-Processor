8a3f5bac9ebbff54f3a2c9dd11ea714422b66b8d
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:46:26 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,40 +1,41 @@
-
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: Image-Processor <bytes|lines|reverse|upper|lower>")
+		fmt.Println("usage: image-processor <file>")
 		os.Exit(1)
 	}
-	mode := os.Args[1]
-	in := ""
-	if len(os.Args) > 2 {
-		in = os.Args[2]
-	}
-	lines := strings.Split(in, "\n")
-	switch mode {
-	case "lines":
-		fmt.Println(len(lines))
-	case "bytes":
-		fmt.Println(len(in))
-	case "upper":
-		fmt.Println(strings.ToUpper(in))
-	case "lower":
-		fmt.Println(strings.ToLower(in))
-	case "reverse":
-		runes := []rune(in)
-		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-			runes[i], runes[j] = runes[j], runes[i]
-		}
-		fmt.Println(string(runes))
-	default:
-		fmt.Fprintln(os.Stderr, "unknown mode:", mode)
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	if len(data) < 8 {
+		fmt.Println("not a valid image")
+		os.Exit(1)
+	}
+	if data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4e && data[3] == 0x47 {
+		w := binary.BigEndian.Uint32(data[16:20])
+		h := binary.BigEndian.Uint32(data[20:24])
+		fmt.Printf("format=PNG width=%d height=%d size=%d\n", w, h, len(data))
+		return
+	}
+	if data[0] == 0xff && data[1] == 0xd8 {
+		fmt.Printf("format=JPEG size=%d bytes\n", len(data))
+		return
+	}
+	if data[0] == 0x47 && data[1] == 0x49 && data[2] == 0x46 {
+		w := binary.LittleEndian.Uint16(data[6:8])
+		h := binary.LittleEndian.Uint16(data[8:10])
+		fmt.Printf("format=GIF width=%d height=%d size=%d\n", w, h, len(data))
+		return
+	}
+	fmt.Println("unknown image format")
+	os.Exit(1)
 }
